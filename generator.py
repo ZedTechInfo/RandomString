4bfe5998cc54a7241979c8f638bb1ceedf24e0cc
Gary Kamoto Mhone <garykamoto@techpay.co.zm>
2024-08-29 16:16:55 +0200

    chore: Fix comment on character length

--- generator.py
+++ generator.py
@@ -5,7 +5,7 @@ import pyperclip
 # Generate random string with letters, digits, and symbols
 characters = string.ascii_letters + string.digits + string.punctuation
 
-# Get and randomise string with character length of 16
+# Get and randomise string with character length from user input
 number_of_characters = int(input("Enter the length of the string: "))
 random_string = "".join(random.choice(characters) for _ in range(number_of_characters))
 
